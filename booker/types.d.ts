a5cfa27655e009a4bb5102393279575368b035ef
AvinashMSangle <avinashMS.3119@proton.me>
2024-12-28 19:33:26 +0000

    Completed

---
+++ booker/types.d.ts
@@ -0,0 +1,25 @@
+interface Book {
+    id: string;
+    title: string;
+    author: string;
+    rating: number;
+  }
+  
+  type Action =
+    | { type: "add"; payload: { title: string; author: string } }
+    | {
+        type: "delete";
+        payload: { id: string };
+      }
+    | {
+        type: "like";
+        payload: { id: string };
+      }
+    | {
+        type: "unlike";
+        payload: { id: string };
+      }
+    | {
+        type: "sort";
+      };
+      
